7f3916e98d8a7ab0a4dbb7e2c7cd2dad7432da9d
Flavien Loiseau <flavien.loiseau@ens-paris-saclay.fr>
2020-05-04 00:36:03 +0200

    Setup base CMake configuration
    It was difficult but it works: https://stackoverflow.com/questions/23850472/how-to-use-sdl2-and-sdl-image-with-cmake

---
+++ CMakeFiles/build/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build/main.dir/src/main.o"
+  "build/main"
+  "build/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
