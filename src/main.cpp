f9bb67fe4b2f98ef84ba998d259c459a8d8ca574
Sangelow <flavien.loiseau@ens-paris-saclay.fr>
2020-05-04 19:49:22 +0200

    Create a SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int main(int, char**){
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+using namespace std;
+
+int main(){
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+		cout << "SDL_Init Error: " << SDL_GetError() << endl;
 		return 1;
 	}
+
+	cout << "Successfull initialisation !" << endl;
+
+	SDL_Window* window = SDL_CreateWindow(
+			"Hello world",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			640,
+			480,
+			SDL_WINDOW_OPENGL);
+
+	SDL_DestroyWindow(window);
+
 	SDL_Quit();
 	return 0;
 }
